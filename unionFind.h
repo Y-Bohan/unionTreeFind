ec59b24fd9a0723b8adb629510ad9a50180db0ab
Y-Bohan <yuebohanc@163.com>
2021-01-15 15:12:33 +0800

    路径压缩优化

--- unionFind.h
+++ unionFind.h
@@ -14,6 +14,7 @@ public:
 	int findGroupCount();		//查找共有多少个分组
 private:
 	int* eleGroup;		//索引存放元素，索引处的值存放该元素的父结点，如eleGroup[5]=6.表示5的父结点是6
+	int* eleSzie;		//索引存放元素,索引处的值存放该元素结点的子树的元素个数，如eleGroup[5]=6.表示5所在的子树共有6个元素
 	int groupCount;		//分组个数
 };
 
@@ -21,9 +22,11 @@ UFTree::UFTree(int N)
 {
 	groupCount = N;
 	eleGroup = new int[N];					//堆上开辟N个空间存放元素的分组初始信息
-	for (int i = 0; i < N; i++)				//初始每个元素单独一组，索引值就是其父结点，也是根结点，即eleGroup[i] == i,元素i的父结点和根结点是本身
+	eleSzie = new int[N];
+	for (int i = 0; i < N; i++)				//初始每个元素单独一组，索引值就是其父结点，也是根结点，即eleGroup[i] == i,元素i的父结点和根结点是本身，每个组内均只有一个元素，eleSzie[i] = 1
 	{
-		eleGroup[i] = i;		
+		eleGroup[i] = i;
+		eleSzie[i] = 1;
 	}
 }
 
@@ -50,7 +53,16 @@ void  UFTree::unionGroup(int x, int y)		//将x所在的组合并到y所在的组
 		return;
 	else
 	{
-		eleGroup[xRoot] = yRoot;		//y所在分组的根结点成为x所在分组的根结点的父结点
+		if (eleSzie[xRoot] > eleSzie[yRoot])
+		{
+			eleGroup[yRoot] = xRoot;		//x所在分组的根结点成为y所在分组的根结点的父结点
+			eleSzie[xRoot] = eleSzie[xRoot] + eleSzie[yRoot];		//由于y的子树挂到了x上，所以x的子树元素个数增加，y的子树元素个数并没有变化
+		}
+		else
+		{
+			eleGroup[xRoot] = yRoot;		//y所在分组的根结点成为x所在分组的根结点的父结点
+			eleSzie[yRoot] = eleSzie[yRoot] + eleSzie[xRoot];
+		}
 		groupCount--;
 	}
 }
@@ -66,6 +78,8 @@ UFTree::~UFTree()		//析构释放堆中内存
 	{
 		delete[] eleGroup;
 		eleGroup = NULL;
+		delete[] eleSzie;
+		eleSzie = NULL;
 	}
 }
 
